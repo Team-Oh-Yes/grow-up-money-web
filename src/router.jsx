3b26324444125d4f10075617036f6fb4d6fe9329
moon <dsmg20090717@dgsw.hs.kr>
2025-10-21 20:41:15 +0900

    feat: 어드민페이지 jsx 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainpages from "./components/Add/Mainpages";
+import Adminpages from "./components/common/AdminComponents/Admin-FT.jsx";
 import Error from "./error/Error";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Mainpages />,
+    element: <Adminpages />,
     errorElement: <Error />,
   },
 ]);

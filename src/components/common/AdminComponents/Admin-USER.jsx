29f371ccde9130724c2c815052492907f94bd2ef
moon <dsmg20090717@dgsw.hs.kr>
2025-10-28 08:49:30 +0900

    feat: hover 수정

--- src/components/common/AdminComponents/Admin-USER.jsx
+++ src/components/common/AdminComponents/Admin-USER.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../css/Admincss/Admin-FT.css';
+import '../../css/Admincss/Admin-USER.css';
 import search from '../../../img/searchIcon.svg';
 import logo from '../../../img/logo.png';
 

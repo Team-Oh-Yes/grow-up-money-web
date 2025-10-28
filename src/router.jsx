780850e8bbe0286d666298acf7796dd81c344b3a
moon <dsmg20090717@dgsw.hs.kr>
2025-10-28 20:53:47 +0900

    feat: adminPage separation

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainpages from "./components/Add/Mainpages";
 import Loginmaincomponents from "./components/common/Loginmaincomponents/Loginmaincomponents";
-import Adminpages from "./components/common/AdminComponents/Admin-USER.jsx";
+import Adminpages from "./components/common/AdminComponents/AdminPages.jsx";
 import Error from "./error/Error";
 import MainTheme from "./components/common/Theme/MainTheme";
 // You need to import the Theme component

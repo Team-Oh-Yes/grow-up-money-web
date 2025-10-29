ac3f8df29978e88a41505c094f9fe5ef6d6a2f3b
moon <dsmg20090717@dgsw.hs.kr>
2025-10-29 20:43:23 +0900

    search logic edit

--- src/components/common/AdminComponents/Admin-FT.jsx
+++ src/components/common/AdminComponents/Admin-FT.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
 import '../../css/Admincss/Admin-FT.css';
+import search from '../../../img/searchIcon.svg';
+
+export default function FtManagement() {
+    return (
+        <div className="search-bar">
+            <div>
+                <img src={search} alt="검색아이콘"></img>
+            </div>
+            <input 
+                placeholder="FT ID를 입력해주세요"
+            />
+        </div>
+    );
+}
 

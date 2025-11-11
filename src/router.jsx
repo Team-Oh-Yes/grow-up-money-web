d8f1ae4564459d62cb50b080ffd77db16dd7ccf4
AN JAE MIN <jaeminan944@icloud.com>
2025-11-11 18:14:55 +0900

    feat: add EULA.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -2,11 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Mainpages from "./components/Add/Mainpages";
 import Error from "./error/Error";
 import Planpages from "./components/common/plancomponents/Planpages";
+import EULA from "./components/common/Serviccenter/EULA";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Mainpages />,
     errorElement: <Error />,
+  },
+  {
+    path: "/eula",
+    element: <EULA />,
+    errorElement: <Error />
   },
   {path:"/plan",
     element :<Planpages />,

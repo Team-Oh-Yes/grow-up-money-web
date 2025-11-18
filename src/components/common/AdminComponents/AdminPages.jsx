c84b7af1afe2b84c96543fae3a2351945792a1a6
han0911 <a01099249559@gmail.com>
2025-11-18 17:51:07 +0900

    fix: import path error

--- src/components/common/AdminComponents/AdminPages.jsx
+++ src/components/common/AdminComponents/AdminPages.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import logo from '../../../img/logo.png';
+import logo from '/src/img/logo.png';
 import UserPage from './Admin-USER';
 import '../../css/Admincss/AdminPages.css';
 import FtPage from './Admin-FT'

922ec72e0dfe27fd37743d2eabd345975cbe3424
hyxx-su <legendary9899@dgsw.hs.kr>
2025-11-18 20:52:13 +0900

    feat :: login 기능 구현 #2

--- src/components/api/axiosInstance.jsx
+++ src/components/api/axiosInstance.jsx
@@ -8,4 +8,29 @@ const axiosInstance = axios.create({
   },
 });
 
+// 요청 인터셉터 - 모든 요청에 쿠키 포함 확인
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.withCredentials = true;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 - 쿠키 저장 확인 로그
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // 쿠키가 제대로 설정되었는지 확인
+    if (response.headers['Set-Cookie']) {
+      console.log('쿠키 설정됨:', response.headers['Set-Cookie']);
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;

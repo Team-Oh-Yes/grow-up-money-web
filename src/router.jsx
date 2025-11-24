52f0ca92a724b064f29c67875f6903581b06d924
han0911 <a01099249559@gmail.com>
2025-11-24 20:51:11 +0900

    fix:error

--- src/router.jsx
+++ src/router.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
   },
   {
-    path: "/login",
+    path: "/roadmap",
     element: <Loginmaincomponents />,
     children: [
       {  

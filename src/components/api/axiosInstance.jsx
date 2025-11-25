5a04db40a28c108c7c75d55013f2f5d9abb47cae
hyxx-su <legendary9899@dgsw.hs.kr>
2025-11-25 19:59:02 +0900

    feat :: https

--- src/components/api/axiosInstance.jsx
+++ src/components/api/axiosInstance.jsx
@@ -19,18 +19,18 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-// 응답 인터셉터 - 쿠키 저장 확인 로그
-axiosInstance.interceptors.response.use(
-  (response) => {
-    // 쿠키가 제대로 설정되었는지 확인
-    if (response.headers['Set-Cookie']) {
-      console.log('쿠키 설정됨:', response.headers['Set-Cookie']);
-    }
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// // 응답 인터셉터 - 쿠키 저장 확인 로그
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     // 쿠키가 제대로 설정되었는지 확인
+//     if (response.headers['Set-Cookie']) {
+//       console.log('쿠키 설정됨:', response.headers['Set-Cookie']);
+//     }
+//     return response;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()  // 이것만 추가!
+  ],
+  server: {
+    https: true
+  }
 })

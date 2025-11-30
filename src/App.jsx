20b5dddb8eabe8a93273ae2bae81b7684c4111e2
moon <dsmg20090717@dgsw.hs.kr>
2025-12-01 08:52:06 +0900

    feat: 병합 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { ToastContainer, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-<<<<<<< HEAD
-=======
 import { Toaster } from "react-hot-toast";
->>>>>>> 56c88d1c53e4dd06d6d1b1ec9a59c5df00d4042b
 
 function App() {
   return (

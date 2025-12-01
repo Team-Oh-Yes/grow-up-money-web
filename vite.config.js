ed1dcd02e3cd4e1fde096f6212b875a436fa6b9b
AN JAE MIN <jaeminan944@icloud.com>
2025-12-01 19:52:14 +0900

    feat: 임시

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
+const keyPath = "./local.growmoney.duckdns.org-key.pem";
+const certPath = "./local.growmoney.duckdns.org.pem";
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    https: mode === "development"
+      ? {
+          key: fs.readFileSync(keyPath),
+          cert: fs.readFileSync(certPath),
+        }
+      : false, 
+    host: "0.0.0.0",
+    port: 5173,
+    hmr: {
+      protocol: 'wss',
+      host: 'local.growmoney.duckdns.org',
+      port: 5173,
+    }
+  },
+}));

ae41165f02c7058203a0bf4552bcde3a614de058
han0911 <a01099249559@gmail.com>
2025-12-01 11:44:54 +0900

    문채원 이슈

--- src/router.jsx
+++ src/router.jsx
@@ -99,7 +99,7 @@ const router = createBrowserRouter([
     errorElement: <Error />
   },
   {
-    path: "/admin/*",
+    path: "/admin",
     element: <Adminpages />,
     errorElement: <Error />,
   },{

83e0539eb3d7cfc7c0a5a35a1b76a97f77ec9639
hyxx-su <legendary9899@dgsw.hs.kr>
2025-12-18 12:08:44 +0900

    feat :: Mainpage 수정 #1

--- src/components/common/maincomponents/Secondmainpages.jsx
+++ src/components/common/maincomponents/Secondmainpages.jsx
@@ -1,14 +1,14 @@
 import "../../css/mainpagescsss/Secondmainpages.css";
-import arrow from "../../../img/MainPage/test1.png";
+import arrow from "../../../img/Icon/r-arrow.svg";
 
 // 개별 뉴스 카드 컴포넌트
-const NewsCard = ({ date, title, description, isSpecial }) => {
+const NewsCard = ({ update, date, title, description, isSpecial }) => {
   return (
     <div className="news-card">
-      <div className={`card-image ${isSpecial ? "highlighted" : ""}`}></div>
+      <div className={`card-image ${isSpecial}`}></div>
       <div className="card-text-box">
         <p className="card-info">
-          시스템 업데이트 <span className="date"> . {date}</span>
+          {update} <span className="date"> . {date}</span>
         </p>
         <h3 className="card-title">{title}</h3>
         <p className="card-description">{description}</p>
@@ -19,40 +19,46 @@ const NewsCard = ({ date, title, description, isSpecial }) => {
 const Secondmainpages = () => {
   const newsData = [
     {
+      update: "시스템 업데이트",
+      date: "2025.12.19",
+      title: "TossPayment X Grow Money",
+      description: "다양한 결제 수단이 추가되었습니다.",
+      isSpecial: "news-card-1",
+    },
+    {
+      update: "정책 업데이트",
+      date: "2025.12.17",
+      title: "플랜 정책 업데이트",
+      description: "사용자 편의를 위해 플랜 가격이 조정되었습니다.",
+      isSpecial: "news-card-2",
+    },
+    {
+      update: "시스템 업데이트",
+      date: "2025.11.20",
+      title: "Pay로 결제하자~!",
+      description: "Pay 결제 수단이 추가되었습니다.",
+      isSpecial: "news-card-3",
+    },
+    {
+      update: "시스템 업데이트",
       date: "2025.09.23",
       title: "디자인 본격적 스타트",
       description: "오늘 2025 나르샤 Oh! Yes팀 디자인 중이라고 합니다!",
-      isSpecial: true, // 특별한 카드를 표시하기 위한 값
+      isSpecial: "news-card-4",
     },
     {
+      update: "공지 사항",
       date: "2025.09.22",
-      title: "프론트엔드 본격적 스타트",
+      title: "베타테스터를 모집합니다!",
       description: "오늘 2025 나르샤 Oh! Yes팀 웹 중이라고 합니다!",
-      isSpecial: false,
+      isSpecial: "news-card-5",
     },
     {
+      update: "시스템 업데이트",
       date: "2025.09.21",
       title: "백엔드 본격적 스타트",
       description: "오늘 2025 나르샤 Oh! Yes팀 서버 개발 중이라고 합니다!",
-      isSpecial: false,
-    },
-    {
-      date: "2025.09.21",
-      title: "백엔드 본격적 스타트",
-      description: "오늘 2025 나르샤 Oh! Yes팀 서버 개발 중이라고 합니다!",
-      isSpecial: false,
-    },
-    {
-      date: "2025.09.21",
-      title: "백엔드 본격적 스타트",
-      description: "오늘 2025 나르샤 Oh! Yes팀 서버 개발 중이라고 합니다!",
-      isSpecial: false,
-    },
-    {
-      date: "2025.09.21",
-      title: "백엔드 본격적 스타트",
-      description: "오늘 2025 나르샤 Oh! Yes팀 서버 개발 중이라고 합니다!",
-      isSpecial: false,
+      isSpecial: "news-card-6",
     },
   ];
 
@@ -68,6 +74,7 @@ const Secondmainpages = () => {
         {newsData.map((news, index) => (
           <NewsCard
             key={index}
+            update={news.update}
             date={news.date}
             title={news.title}
             description={news.description}
